323e1a6dc9273ee191a1f6721d9900a9a13993c5
wbvanderbroek <wbvanderbroek@st.noorderpoort.nl>
2025-05-08 22:24:39 +0200

    Fix: Telling python what port to use

--- bingo-backend/app.py
+++ bingo-backend/app.py
@@ -204,4 +204,4 @@ def delete_bingo_element(id):
     return jsonify({'message': 'Element deleted'}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
